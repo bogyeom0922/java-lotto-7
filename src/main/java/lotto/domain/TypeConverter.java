71908b7d9a284f06c803ba65ac7c8dd08f4a39c8
bogyeom0922 <pbk0398@gmail.com>
2025-05-05 15:27:08 +0900

    Refactor: 검증기 필드 주입 및 메서드 간소화

--- src/main/java/lotto/domain/TypeConverter.java
+++ src/main/java/lotto/domain/TypeConverter.java
@@ -6,23 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TypeConverter {
+    private final WinningNumbers winningNumbers = new WinningNumbers();
 
     public List<Integer> convertToList(String inputWinningNumbers) {
-        WinningNumbers winningNumbers = new WinningNumbers();
-
         winningNumbers.validateEmptyString(inputWinningNumbers);
-
         List<Integer> inputWinningNumbersList = new ArrayList<>();
-
         for (String number : inputWinningNumbers.split(",")) {
             String num = number.trim();
             winningNumbers.validateNumberIsInteger(num);
-            int winningNumber = Integer.parseInt(number.trim());
+            int winningNumber = Integer.parseInt(num);
             inputWinningNumbersList.add(winningNumber);
         }
-
         return inputWinningNumbersList;
     }
-
-
 }
